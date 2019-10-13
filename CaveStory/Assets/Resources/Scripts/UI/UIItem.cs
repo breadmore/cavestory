d466115c2861bc16db2ed71c737d10eae5cf3a12
Backsun Sim <zuidrijk@gmail.com>
2019-10-13 22:06:19 +0900

    2019-10-13 가구/장비 관련 클래스 추가

---
+++ CaveStory/Assets/Resources/Scripts/UI/UIItem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIItem : MonoBehaviour
+{
+    private Item itemData;
+
+    public void Init(Item data)
+    {
+        itemData = data;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
