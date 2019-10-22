1dd66905001263fe3f2be388b8fa6c5980732142
Myoryung <iove_v@naver.com>
2019-10-22 21:08:54 +0900

    Low Poly Asset 등록

---
+++ CaveStory/Assets/Resources/Scripts/Furniture/Furniture.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TYPE_FURNITURE
+{
+    DUNGEON_HALL,
+    FARM,
+    BED,
+    TENT,
+    CRYSTAL_BALL,
+    PICKAXE,
+    ANVIL,
+    TRAINING_CENTER,
+    TELEPORT
+}
+
+public enum EFFECT_FURNITURE
+{
+    POPULATION,
+    FOOD,
+    SOLDIER,
+    MANA,
+    DIGGING,
+    MAKE_EQUIPMENT,
+    GET_JOB
+}
+
+public class Furniture : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
