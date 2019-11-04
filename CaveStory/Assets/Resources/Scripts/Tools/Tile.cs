fa3c81564f8e25cbcbcaa3d9327927dd9e6ac3c4
Myoryung <iove_v@naver.com>
2019-11-04 23:35:34 +0900

    .gitignore and MapTools

---
+++ CaveStory/Assets/Resources/Scripts/Tools/Tile.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    private ToolsManager TM;
+    public GameObject Tree;
+    public GameObject Stone;
+    public int currentX;
+    public int currentY;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        TM = GameObject.Find("ToolsManager").GetComponent<ToolsManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        switch (TM.selectObject)
+        {
+            case ToolsManager.TileState.EMPTY:
+                break;
+            case ToolsManager.TileState.TREE:
+                if (Stone.activeSelf)
+                    Stone.SetActive(false);
+                else if (Tree.activeSelf)
+                    Tree.SetActive(false);
+                else
+                    Tree.SetActive(true);
+                break;
+            case ToolsManager.TileState.STONE:
+                if (Tree.activeSelf)
+                    Tree.SetActive(false);
+                else if (Stone.activeSelf)
+                    Stone.SetActive(false);
+                else
+                    Stone.SetActive(true);
+                break;
+        }        
+    }
+}
---
+++ CaveStory/Assets/Resources/Scripts/Tools/ToolsManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolsManager : MonoBehaviour
+{
+    public int MapX = 1;
+    public int MapY = 1;
+    public TileState[, ] Map;
+    public Transform Tools;
+    public GameObject SettingTile;
+    public enum TileState { EMPTY, TREE, STONE};
+    public TileState selectObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Map = new TileState[MapX, MapY];
+        for(int i=0; i<MapX; i++)
+        {
+            for(int j=0; j<MapY; j++)
+            {
+                Instantiate(SettingTile, new Vector3(i * 1.01f, 0, j * 1.01f), Tools.rotation, Tools);
+                Tools.GetChild(i * MapX + j).GetComponent<Tile>().currentX = i;
+                Tools.GetChild(i * MapX + j).GetComponent<Tile>().currentY = j;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeSelectObject(int index)
+    {
+        switch(index)
+        {
+            case 1:
+                if (selectObject == TileState.TREE)
+                    selectObject = TileState.EMPTY;
+                selectObject = TileState.TREE;
+                break;
+            case 2:
+                if (selectObject == TileState.STONE)
+                    selectObject = TileState.EMPTY;
+                selectObject = TileState.STONE;
+                break;
+        }
+    }
+}
